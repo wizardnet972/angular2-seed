ffef47372c2f5b418603d260b17df95bf1dfa6e2
WizardNet972 <wizardnet972@gmail.com>
2016-10-06 13:31:17 +0300

    update angular2-seed and @ngrx

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -4,6 +4,22 @@ import { argv } from 'yargs';
 
 import { Environments, InjectableDependency } from './seed.config.interfaces';
 
+/************************* DO NOT CHANGE ************************
+ *
+ * DO NOT make any changes in this file because it will
+ * make your migration to newer versions of the seed harder.
+ *
+ * Your application-specific configurations should be
+ * in project.config.ts. If you need to change any tasks
+ * from "./tasks" overwrite them by creating a task with the
+ * same name in "./projects". For further information take a
+ * look at the documentation:
+ *
+ * 1) https://github.com/mgechev/angular2-seed/tree/master/tools
+ * 2) https://github.com/mgechev/angular2-seed/wiki
+ *
+ *****************************************************************/
+
 /**
  * The enumeration of available environments.
  * @type {Environments}
@@ -273,11 +289,6 @@ export class SeedConfig {
    * @type {string}
    */
   VERSION_NODE = '4.0.0';
-
-  /**
-   * The ruleset to be used by `codelyzer` for linting the TypeScript files.
-   */
-  CODELYZER_RULES = customRules();
 
   /**
    * The flag to enable handling of SCSS files
@@ -580,15 +591,6 @@ function filterDependency(env: string, d: InjectableDependency): boolean {
 function appVersion(): number | string {
   var pkg = require('../../package.json');
   return pkg.version;
-}
-
-/**
- * Returns the linting configuration to be used for `codelyzer`.
- * @return {string[]} The list of linting rules.
- */
-function customRules(): string[] {
-  var lintConf = require('../../tslint.json');
-  return lintConf.rulesDirectory;
 }
 
 /**
